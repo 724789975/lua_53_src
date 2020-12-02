4e933ae6b210f8cb2478892f276a72cacd24222a
724789975@qq.com <724789975@qq.com>
2020-12-02 21:45:38 +0800

    注释修改

--- lstring.c
+++ lstring.c
@@ -105,10 +105,10 @@ void luaS_resize (lua_State *L, int newsize) {
 }
 
 
-/*
-** Clear API string cache. (Entries cannot be empty, so fill them with
-** a non-collectable string.)
-** 清楚缓存
+/**
+ * Clear API string cache. (Entries cannot be empty, so fill them with
+ * a non-collectable string.)
+ * 清楚缓存
 */
 void luaS_clearcache (global_State *g) {
   int i, j;
@@ -120,9 +120,9 @@ void luaS_clearcache (global_State *g) {
 }
 
 
-/*
- ** Initialize the string table and the string cache
- ** 初始化字符串链表和字符串缓存
+/**
+ * Initialize the string table and the string cache
+ * 初始化字符串链表和字符串缓存
  */
 void luaS_init (lua_State *L) {
   global_State *g = G(L);
@@ -208,11 +208,11 @@ static TString *internshrstr (lua_State *L, const char *str, size_t l) {
 }
 
 
-/*
- ** new string (with explicit length)
- ** 创建一个存新的字符串,不带缓存
- ** 字符串不能超过最大限制
- ** 新的字符串会memcpy拷贝一个副本,挂载到TString结构上
+/**
+ * new string (with explicit length)
+ * 创建一个存新的字符串,不带缓存
+ * 字符串不能超过最大限制
+ * 新的字符串会memcpy拷贝一个副本,挂载到TString结构上
  */
 TString *luaS_newlstr (lua_State *L, const char *str, size_t l) {
   if (l <= LUAI_MAXSHORTLEN)  /* short string? */
@@ -228,20 +228,20 @@ TString *luaS_newlstr (lua_State *L, const char *str, size_t l) {
 }
 
 
-/*
- ** Create or reuse a zero-terminated string, first checking in the
- ** cache (using the string address as a key). The cache can contain
- ** only zero-terminated strings, so it is safe to use 'strcmp' to
- ** check hits.
- ** 创建一个新的字符串,带缓存方式
- ** 会调用luaS_newlstr方法
- ** 1. 先通过字符串,获取字符串hash值
- ** 2. 通过hash值取字符串,如果相同的字符串已经存在,则复用
- ** 3. 否则创建一个新的字符串
- **
- ** 字符串Table表,通过字符串的hash值找到list
- ** 但是list长度是STRCACHE_M=2,list比较小,估计作者认为hash冲突的概率会非常小
- ** 同时每次都会将最早的元素element淘汰出去
+/**
+ * Create or reuse a zero-terminated string, first checking in the
+ * cache (using the string address as a key). The cache can contain
+ * only zero-terminated strings, so it is safe to use 'strcmp' to
+ * check hits.
+ * 创建一个新的字符串,带缓存方式
+ * 会调用luaS_newlstr方法
+ * 1. 先通过字符串,获取字符串hash值
+ * 2. 通过hash值取字符串,如果相同的字符串已经存在,则复用
+ * 3. 否则创建一个新的字符串
+ *
+ * 字符串Table表,通过字符串的hash值找到list
+ * 但是list长度是STRCACHE_M=2,list比较小,估计作者认为hash冲突的概率会非常小
+ * 同时每次都会将最早的元素element淘汰出去
  */
 TString *luaS_new (lua_State *L, const char *str) {
   unsigned int i = point2uint(str) % STRCACHE_N;  /* hash */
